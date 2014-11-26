9ed027894b5bfa57e5b10ffcd570ffc308ef8843
andrew.chaa <andrew.chaa@huddle.com>
2014-11-26 14:35:24 +0000

    initial commit

---
+++ Robots.Tests/PlaygroundTests.cs
@@ -0,0 +1,21 @@
+﻿using Machine.Specifications;
+
+namespace Robots
+{
+    public class PlaygroundTests
+    {
+        [Subject("Playgroud")]
+        public class When_the_ground_is_created
+        {
+            private static Ground _ground;
+            private static int _width = 5;
+            private static int _height = 5;
+            
+            Because of = () => _ground = new Ground(_width, _height);
+
+            It should_have_set_the_correct_width = () => _ground.Width.ShouldEqual(_width);
+            It should_have_set_the_correct_height = () => _ground.Width.ShouldEqual(_height);
+        }
+    }
+
+}
---
+++ Robots/Ground.cs
@@ -0,0 +1,16 @@
+﻿namespace Robots
+{
+    public class Ground
+    {
+        private readonly string[,] _ground;
+
+        public Ground(int width, int height)
+        {
+            _ground = new string[width, height];
+        }
+
+        public int Width { get { return _ground.GetLength(0); } }
+        public int Height { get { return _ground.GetLength(1); } }
+
+    }
+}
